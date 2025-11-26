a8505fcf189bad951832e7b974cd5c3e14c01f3d
wassimachouri <wassimachouri22@gmail.com>
2025-11-26 20:50:57 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Welcome from './components/Welcome';
+function App() {
+  return (
+    <div className="App">
+      <Welcome name="Ali"/>
+    </div>
+  );
+}
+export default App;
---
+++ src/components/Welcome.js
@@ -0,0 +1,4 @@
+function Welcome(props){
+return <h1>welcome home BRO, {props.name}</h1>;
+}
+export default Welcome;
